0a6471511417d7bc8ea5663c169ba100333ab2eb
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-29 16:14:09 -0800

    test for parse

---
+++ test/parse.js
@@ -0,0 +1,19 @@
+require('tape')('parse', function (t) {
+
+  var bucket = require('..')
+
+  var b = bucket('bucket')
+  var k = b('hello')
+
+  t.deepEqual(b.parse(b('hello')), {
+    prefix: ['bucket'],
+    key: 'hello'
+  })
+
+  t.deepEqual(b.parse(b(['hello', 'whatever'])), {
+    prefix: ['bucket'],
+    key: ['hello', 'whatever']
+  })
+
+})
+
