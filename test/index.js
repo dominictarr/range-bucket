63f70c26e3b4b9216a5691445f5868404deeb217
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-02 23:00:26 -0800

    improve tests

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+var test = require('tape')
 
 var bucket = require('..')
 var assert = require('assert')
@@ -9,10 +10,12 @@ function random(n) {
   return ( n ? a + random(n - 1) : a )
 }
 
-var b = bucket('BUCKET')
-
-assert.equal(typeof b, 'function')
-assert.equal(typeof b.range, 'function')
+test('simple', function (t) {
+  var b = bucket('BUCKET')
+  t.equal(typeof b, 'function')
+  t.equal(typeof b.range, 'function')
+  t.end()
+})
 
 function within(range, key) {
   return range.start <= key && key < range.end
@@ -22,67 +25,75 @@ function times (n, iter) {
   while(n-- > 0) iter(n)
 }
 
-times(100, function () {
-  var b  = bucket(random(6))
-  var _b = bucket(random(6))
-  var K  = random(6)
-  var k  = b (K)
-  var _k = _b(K)
+test('range.within', function (t) {
+  times(10, function () {
+    var b  = bucket(random(6))
+    var _b = bucket(random(6))
+    var K  = random(6)
+    var k  = b (K)
+    var _k = _b(K)
 
-  console.log(b.parse(k))
+    console.log(b.parse(k))
 
-  assert( b.range().within(k))
-  //keys for a different bucket *must* fall outside the range.
-  assert( !_b.range().within(k) )
-  assert( _b.range().within(_k) )
+    t.ok( b.range().within(k),   'within')
+    //keys for a different bucket *must* fall outside the range.
+    t.ok( !_b.range().within(k), '!within')
+    t.ok( _b.range().within(_k), 'within')
+  })
 })
 
-times(100, function () {
-  var b  = bucket(random(6))
-  var _b = bucket(random(6))
-  var K  = random(6)
-  var K2  = random(6)
-  var k  = b (K, K2)
-  var _k = _b(K, K2)
+test('range.within 2', function (t) {
+  times(10, function () {
+    var b  = bucket(random(6))
+    var _b = bucket(random(6))
+    var K  = random(6)
+    var K2  = random(6)
+    var k  = b (K, K2)
+    var _k = _b(K, K2)
 
-  assert( within(b.range(), k) )
-  //keys for a different bucket *must* fall outside the range.
-  assert( !within(_b.range(),  k) )
-  assert(  within(_b.range(), _k) )
+    t.ok( within(b.range(), k), 'within')
+    //keys for a different bucket *must* fall outside the range.
+    t.ok( !within(_b.range(),  k), '!within')
+    t.ok(  within(_b.range(), _k), 'within')
+    t.end()
+  })
 })
 
+test('range.within [groups]', function (t) {
+  times(10, function () {
+    var b  = bucket(random(6))
+    var big = random(4), little = random(4), _little = random(4)
 
-times(100, function () {
-  var b  = bucket(random(6))
-  var big = random(4), little = random(4), _little = random(4)
+    assert(within(b.range([big, true]), b([big, little])))
+    assert(within(b.range([big, little]), b([big, little])))
+    assert(within(b.range([true, true]), b([big, little])))
 
-  assert(within(b.range([big, true]), b([big, little])))
-  assert(within(b.range([big, little]), b([big, little])))
-  assert(within(b.range([true, true]), b([big, little])))
+    console.log(b.parse(b([big, little])))
 
-  console.log(b.parse(b([big, little])))
-
-
-  assert(!within(b.range([big, little]), b([big, _little])))
-  assert(within(b.range([big, true]), b([big, _little])))
-  assert(within(b.range([true, true]), b([big, _little])))
+    t.ok(!within(b.range([big, little]), b([big, _little])))
+    t.ok(within(b.range([big, true]), b([big, _little])))
+    t.ok(within(b.range([true, true]), b([big, _little])))
+    t.end()
+  })
 })
 
-times(10, function () {
-  var b  = bucket(random(6), 'sub')
-  var big = random(4), little = random(4), _little = random(4)
+test('range.within [groups, true]', function (t) {
+  times(10, function () {
+    var b  = bucket(random(6), 'sub')
+    var big = random(4), little = random(4), _little = random(4)
 
-  //console.log(b.range([big, true]) )
-//  console.log(b([big, little]))
+    //console.log(b.range([big, true]) )
+  //  console.log(b([big, little]))
 
-  console.log(b.parse(b([big, little])))
+    console.log(b.parse(b([big, little])))
 
-  assert(b.range([big, true]).within(b([big, little])))
-  assert(b.range([big, little]).within(b([big, little])))
-  assert(b.range([true, true]).within(b([big, little])))
+    t.ok(b.range([big, true]).within(b([big, little])))
+    t.ok(b.range([big, little]).within(b([big, little])))
+    t.ok(b.range([true, true]).within(b([big, little])))
 
-  assert(!b.range([big, little]).within(b([big, _little])))
-  assert(b.range([big, true])   .within( b([big, _little])))
-  assert(b.range([true, true])  .within(b([big, _little])))
+    t.ok(!b.range([big, little]).within(b([big, _little])))
+    t.ok(b.range([big, true])   .within( b([big, _little])))
+    t.ok(b.range([true, true])  .within(b([big, _little])))
+    t.end()
+  })
 })
-
